3fda058a2416934083dfe69d6088c070db8c1247
Abdul Ahad Butt <abdulahadbutt@example.com>
2026-07-05 20:20:48 -0500

    fix: Allow x-admin-key header in CORS preflight

--- src/worker.js
+++ src/worker.js
@@ -1,7 +1,7 @@
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization, x-admin-key",
 };
 
 export default {
@@ -15,7 +15,7 @@ export default {
         headers: {
           "Access-Control-Allow-Origin": "*",
           "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-          "Access-Control-Allow-Headers": "Content-Type, Authorization",
+          "Access-Control-Allow-Headers": "Content-Type, Authorization, x-admin-key",
         }
       });
     }
